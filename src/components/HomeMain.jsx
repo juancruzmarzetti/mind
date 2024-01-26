8bb732c1247f46ddb010e2d3c15aa23e7fdcbc0e
juancruzmarzetti <naujmarz@gmail.com>
2024-01-26 00:56:15 -0300

    home-main-first-article

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -4,10 +4,14 @@ export default function HomeMain() {
 
     return (
         <>
-            <main>
+            <main className="w-screen">
                 <section>
-                    <article>
-
+                    <article className="font-dm text-white bg-black flex">
+                        <div className="ml-[9%] w-[60%] mt-36 pb-[120px]">
+                            <h1 className="text-[75px] leading-none">✱ <span className="text-[#DBB2FF]">Agencia creativa</span> de Growth Marketing</h1>
+                            <h3 className="mt-12 text-[20px] w-[50%]">Soluciones digitales para tu negocio. Diseñamos estrategias de marketing innovadoras</h3>
+                        </div>
+                        <img className="absolute w-[22vw] ml-[78vw] mt-56" src="logomind.png" alt="Logo de la Agencia creativa de Growth Marketing"/>
                     </article>
                     <article>
 
